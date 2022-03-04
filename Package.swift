402f21c84f7df723b635c24335bdd1df09fd232c
Paavo Moilanen <paavo.moilanen@tuup.fi>
2022-03-04 16:26:38 +0200

    Trying to get the pure playgrounds to work with SPM settings

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CAudioKitEX",
-    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
+    platforms: [.macOS(.v11), .iOS(.v11), .tvOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,14 +14,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        //.package(url: "https://github.com/PMoilanen/AudioKitEX.git", from: "5.3.0"),
+//        .package(url: "https://github.com/PMoilanen/AudioKitEX.git", from: "5.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CAudioKitEX",
-            dependencies: []),
+            name: "CAudioKitEX", cxxSettings: [.headerSearchPath(".")]),
         .testTarget(
             name: "CAudioKitEXTests",
             dependencies: []),
